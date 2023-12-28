e4e3f2f1e7184719ab2695c8138b1efe26bd517c
GarethLlewelyn <gareth873@gmail.com>
2023-12-28 16:45:21 +0000

    Added functionality for the 4 subclasses

--- ApplicationDriver.java
+++ ApplicationDriver.java
@@ -11,28 +11,69 @@ public class ApplicationDriver {
     public static boolean LoggedIn = false;
     static String[] UserResult;
     
+	public static String[][] Hotels; //Tourist global variables
+	public static String[][] Resteraunts;
+	public static String[][] WhatsOn;
+	public static String[][] Accomidation; //Student global variables
+	public static String[][] UniCol;
+	public static String[][] JobPost;//Job global variables
+	public static String[][] Volunteering;
+	public static String[][] RentableSpaces;//Business global variables
+	public static String[][] NewBuisnesses;
+	public static String[][] BusinessNews;
+	
+	
+	public static String[][] HotelsTable; 
+	public static String[][] ResterauntsTable;
+	public static String[][] WhatsOnTable;
+	public static String[][] AccomidationTable; //Student global variables
+	public static String[][] UniColTable;
+	public static String[][] JobPostTable;//Job global variables
+	public static String[][] VolunteeringTable;
+	public static String[][] RentableSpacesTable;//Business global variables
+	public static String[][] NewBuisnessesTable;
+	public static String[][] BusinessNewsTable;
+	public static String[][] ApprentishipTable;
+    
     
 	public static void main(String[] args) throws SQLException, IOException {
-		DBInstance = new DatabaseDriver("root", "", "127.0.0.1", "smartcity", 3306);
+		DBInstance = new DatabaseDriver("root", "", "127.0.0.1", "smartcity", 3306); //Start DB Intance for global access
 		
+		
+
+		
+        Tourism tourism = new Tourism();  // Retrieve Tourism information from database and keep as global variable
+        tourism.RetrieveTable();
+        tourism.ConvertTable();
+        HotelsTable = tourism.HotelsTable;
+        ResterauntsTable = tourism.ResterauntsTable;
+        WhatsOnTable = tourism.WhatsOnTable;
+        Student student = new Student();  // Retrieve Student information from database and keep as global variable
+        student.RetrieveTable();
+        student.ConvertTable();
+        AccomidationTable = student.AccomidationTable;
+        UniColTable = student.UniColTable;
+        JobAplicants Job = new JobAplicants();  // Retrieve Job information from database and keep as global variable
+        Job.RetrieveTable();
+        Job.ConvertTable();
+        JobPostTable = Job.JobPostTable;
+        VolunteeringTable = Job.VolunteeringTable;
+        Buisness business = new Buisness();  // Retrieve Business information from database and keep as global variable
+        business.RetrieveTable();
+        business.ConvertTable();
+        RentableSpacesTable = business.RentableSpacesTable;
+        NewBuisnessesTable = business.NewBuisnessesTable;
+        BusinessNewsTable = business.BusinessNewsTable;
+
 		
 		if(!LoggedIn) {
 
 			//new GUI();
-			GUI.Login();
+			GUI.SmartCityPage();
 		}else {
 			
 			System.out.println("SIGN IN SUCCESS");
 		}
-		
-		
-		//String[][] TempHotel = DBInstance.Retrievetable("hotels");
-        Tourism tourism = new Tourism(null, null, null);  // Pass null since data will be retrieved later
-        tourism.RetrieveTable();
-
-		
-		
-		
 		
 
 		
